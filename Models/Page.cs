172aac7b7ad2f8d8e8ade8e98121813da76a25e3
Byron Custodio <byron.custodio@gmail.com>
2024-03-01 17:46:03 -0800

    Fixing nullable types

--- Models/Page.cs
+++ Models/Page.cs
@@ -4,15 +4,9 @@ namespace NotionSDK.Models;
 
 public class Page
 {
-    public Page(string o, string id)
-    {
-        Object = o;
-        Id = id;
-    }
-
     [JsonProperty("object")]
-    public string Object { get; set; }
+    public string? Object { get; set; }
 
     [JsonProperty("id")]
-    public string Id { get; set; }
+    public string? Id { get; set; }
 }

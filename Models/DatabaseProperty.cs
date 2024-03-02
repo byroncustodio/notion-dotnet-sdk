41de83939f3a86a39b1587fbab4253014cef854e
Byron Custodio <byron.custodio@gmail.com>
2024-03-01 17:42:04 -0800

    Fixing nullable types

--- Models/DatabaseProperty.cs
+++ Models/DatabaseProperty.cs
@@ -4,19 +4,12 @@ namespace NotionSDK.Models;
 
 public class DatabaseProperty
 {
-    public DatabaseProperty(string id, string name, string type)
-    {
-        Id = id;
-        Name = name;
-        Type = type;
-    }
-
     [JsonProperty("id")]
-    public string Id { get; set; }
+    public string? Id { get; set; }
 
     [JsonProperty("name")]
-    public string Name { get; set; }
+    public string? Name { get; set; }
 
     [JsonProperty("type")]
-    public string Type { get; set; }
+    public string? Type { get; set; }
 }
--- Models/PageProperty.cs
+++ Models/PageProperty.cs
@@ -4,15 +4,9 @@ namespace NotionSDK.Models;
 
 public class PageProperty
 {
-    public PageProperty(string id, string type)
-    {
-        Id = id;
-        Type = type;
-    }
-
     [JsonProperty("id")]
-    public string Id { get; set; }
+    public string? Id { get; set; }
 
     [JsonProperty("type")]
-    public string Type { get; set; }
+    public string? Type { get; set; }
 }

33cc6ec3b63b32c7787bd60f8d6f43ae6985b327
Byron Custodio <byron.custodio@gmail.com>
2024-03-06 19:23:51 -0800

    Fixing structure of number property

--- Models/Property/Number.cs
+++ Models/Property/Number.cs
@@ -5,25 +5,11 @@ namespace NotionSDK.Models.Property;
 public class Number : PageProperty
 {
     [JsonConstructor]
-    public Number(NumberData data)
-    {
-        Data = data;
-    }
-
     public Number(decimal value)
     {
-        Data = new NumberData
-        {
-            Value = value
-        };
+        Value = value;
     }
 
     [JsonProperty("number")]
-    public NumberData Data { get; set; }
-}
-
-public class NumberData
-{
-    [JsonProperty("number", NullValueHandling = NullValueHandling.Ignore)]
     public decimal Value { get; set; }
 }
